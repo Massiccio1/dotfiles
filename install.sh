58307e4b19a8e54c9ed8de2a0ed7dd1253dfdd55
Massiccio1 <girardellimassimo@gmail.com>
2024-01-30 16:25:08 +0100

    fixed isntall typo

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-echo "if [ -f ~/.bash_aliases ]; then . ~/.bash_aliases fi">>. ~/.bashrc
+echo "if [ -f ~/.bash_aliases ]; then . ~/.bash_aliases fi">>~/.bashrc
+. ~/bashrc
 rm README.md
 rm install.sh
