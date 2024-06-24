559a27aad8128db44693a56de137770c36423ac0
Massiccio1 <girardellimassimo@gmail.com>
2024-06-24 17:54:17 +0200

    rc6 script fix, removed har dcoded user in del

--- kde/scripts/deltmp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/rm -rf /home/massimo/Downloads/delete/*
--- scripts/kde.sh
+++ scripts/kde.sh
@@ -20,9 +20,16 @@ cd scripts
 cp -rf * ~/scripts
 
 
+# if [ -f "$HOME/scripts/deltmp.sh" ]; then
+#     $SUDO ln -s $HOME/scripts/deltmp.sh /etc/rc6.d/k99deltmp
+# fi
 
-if [ -f "$HOME/scripts/deltmp.sh" ]; then
-    $SUDO ln -s $HOME/scripts/deltmp.sh /etc/rc6.d/k99deltmp
-fi
+echo "#\!/bin/bash" > $HOME/scripts/deltmp.sh
+echo "/usr/bin/rm -rf $HOME/Downloads/delete/*" >> $HOME/scripts/deltmp.sh
+
+chmod +x  $HOME/scripts/deltmp.sh
+
+
+$SUDO ln -s $HOME/scripts/deltmp.sh /etc/rc6.d/K99deltmp
 
 cd ../..
