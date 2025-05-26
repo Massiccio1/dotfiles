2579eacc47f5fdb542421b4dc5800051e5a9bbef
Massimo Girardelli <massimo.girardelli@starsystem.biz>
2025-05-26 11:36:38 +0200

    default autocommit

--- scripts/install-auto.sh
+++ scripts/install-auto.sh
@@ -74,6 +74,38 @@ function install_fastfetch(){
 }
 # install
 
+function install_font(){
+
+    $SUDO apt install -y unzip fontconfig
+    BASE_PATH=$(pwd)
+    mkdir -p work
+    cd work
+
+    # curl -L -o JetBrainsMono.zip https://download-cdn.jetbrains.com/fonts/JetBrainsMono-2.304.zip
+    # unzip -o JetBrainsMono.zip
+    # cd fonts/ttf
+    # $SUDO mkdir -p /usr/local/share/fonts/j
+    # $SUDO mv * /usr/local/share/fonts/j
+
+
+
+    #now firamono
+
+    curl -L -o FiraMono.zip "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraMono.zip"
+    unzip FiraMono.zip
+
+    $SUDO mkdir -p /usr/share/fonts/opentype
+    $SUDO cp *.otf /usr/share/fonts/opentype
+    $SUDO fc-cache -fv
+
+
+    cd $BASE_PATH
+    rm -rf work
+    fc-cache -f -v
+
+
+}
+
 
 
 startpwd=$(pwd)
@@ -84,6 +116,9 @@ install_staship
 install_zsh_scripts
 
 install_fastfetch
+
+install_font
+
 
 RED='\033[0;31m'
 NC='\033[0m' # No Color
